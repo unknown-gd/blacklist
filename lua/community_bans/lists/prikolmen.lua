ca9ff75f416a44fc948d2b24b3855cd7b2955672
PrikolMen:-b <44779902+PrikolMen@users.noreply.github.com>
2022-10-22 09:29:23 +0400

    Update prikolmen.lua

--- lua/community_bans/lists/prikolmen.lua
+++ lua/community_bans/lists/prikolmen.lua
@@ -1,3 +1,3 @@
 Name = 'PrikolMen\'s Little Blacklist'
-Source = ''
+Source = 'https://raw.githubusercontent.com/PrikolMen/gmod_blacklist/main/blacklist.txt'
 Reason = 'Если вы видите это сообщение, значит, вы являетесь почетным членом моего черного списка и поэтому вам заблокирован доступ к этому серверу <3'
