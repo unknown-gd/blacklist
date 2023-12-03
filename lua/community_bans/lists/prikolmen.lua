009005911524bd6e70c44ee4df3137b2845199cd
Unknown Developer <44779902+PrikolMen@users.noreply.github.com>
2023-12-03 18:16:26 +0400

    Delete lua directory

--- lua/community_bans/lists/prikolmen.lua
+++
@@ -1,3 +0,0 @@
-Name = 'PrikolMen\'s Little Blacklist'
-Source = 'https://raw.githubusercontent.com/PrikolMen/gmod_blacklist/main/blacklist.txt'
-Reason = 'Если вы видите это сообщение, значит, вы являетесь почетным членом моего черного списка и поэтому вам заблокирован доступ к этому серверу <3'
